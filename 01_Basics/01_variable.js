c53936cdf6b24d89d0b1f739b0929e00bcd7498b
akash284374 <121781009+akash284374@users.noreply.github.com>
2024-08-10 16:39:00 +0000

    pr01

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,24 @@
+const accountId=144553
+let accountEmail="akash@google.com"
+var accountPass="123"
+accountCity="Jaipur"
+
+// accountId=2
+accountEmail="aman@google.com"
+accountPass="321"
+accountCity="Bihar"
+let accountState;
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPass);
+// console.log(accountCity);
+
+// console.table([accountId,accountEmail,accountPass,accountCity])
+console.table([accountEmail,accountId,accountPass,accountCity])
+
+/*
+  prefer not to use var 
+  because of block scope and functional scope
+*/
+
