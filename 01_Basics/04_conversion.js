1c8a0014268219bfdd4f41aa773456ff20b8be69
akash284374 <121781009+akash284374@users.noreply.github.com>
2024-08-10 18:53:10 +0000

    conversion

---
+++ 01_Basics/04_conversion.js
@@ -0,0 +1,30 @@
+// console.log(2>2);
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// false
+// true
+// false
+// false
+// true
+
+
+// console.log("2">1);
+// console.log("02">1);
+// true
+// true
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//DataType Check (===)
+
+console.log(2==="2");
+console.log(2===2);
+
+
+
+
+
