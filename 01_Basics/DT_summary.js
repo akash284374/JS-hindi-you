1c2002d5f4dee886dc391276c62b89bdb4ea0e01
akash284374 <121781009+akash284374@users.noreply.github.com>
2024-08-10 19:35:21 +0000

    DType

---
+++ 01_Basics/DT_summary.js
@@ -0,0 +1,41 @@
+//Primitive 
+//7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
+
+// const score=100
+// const scoreValue=100.3
+// const isLoggedIn=false
+// const outsideTemp=null
+// // let userEmail;  --->this 
+// let userEmail=undefined;//-->and this is same
+
+
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+
+// console.log(id===anotherId);
+
+
+
+//Reference (Non Primitive)
+//Array,Objects,Functions
+
+
+//array
+const heroes=["Shaktiman","naagraj","doga"];
+
+//obj
+
+let myObj={
+    name:"akash",
+    age:22
+}
+
+// {
+//     name:"akash"
+//     age: 22
+// }
+
+const myFunction=function(){
+    console.log("Hello Duniya");
+}
+console.log(typeof myFunction)
