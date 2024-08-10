2ac9d0e45b1170663689c6c010aeb4747ca7732b
akash284374 <121781009+akash284374@users.noreply.github.com>
2024-08-10 17:08:47 +0000

    datatypes

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newew version
+
+// alert(3+3)    Gives error  ,because we are using node js not browser
+
+console.log(3+4)
+console.log("AKASH")
+
+let name="akash"
+let age=18
+let isLoggedIn=false
+// let state=null
+let state;
+
+
+//number=>2 to power 53
+//bigint
+// string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+
+// object
+
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
+
+
+
