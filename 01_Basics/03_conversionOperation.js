a7dee8b465efc0dd98962681e335ffe865ba151f
akash284374 <121781009+akash284374@users.noreply.github.com>
2024-08-10 18:36:22 +0000

    conversion2

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -25,7 +25,41 @@ null=0,undefined->NaN
 // let boolIsLoggedIn=Boolean(isLoggedIn)
 // console.log(boolIsLoggedIn);
 
-let someNumber=33;
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let someNumber=33;
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+//****operations****
+// let value=3
+// let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1="hello"
+// let str2="akash"
+// let str3=str1+str2;
+// console.log(str3)
+
+// console.log("1"+2) //12
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter=100
+// gameCounter++
+// console.log(gameCounter);
+
+console.log(gameCounter++)
+console.log(++gameCounter)
+
+
+
+
+
