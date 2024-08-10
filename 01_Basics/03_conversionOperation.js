b453a861fdeb074aa92590b30b0c15d333fe530d
akash284374 <121781009+akash284374@users.noreply.github.com>
2024-08-10 17:58:36 +0000

    Conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score=33
+// // const {score}=req.body
+// console.log(typeof score);
+
+// let score="33";
+// console.log(typeof score);
+
+// let valInNumber=Number(score);
+// console.log(typeof valInNumber);
+// console.log(valInNumber); //33->33, 33abc->NaN,null=0,undefined->NaN
+
+/*
+"33"=>33
+"33abc"=>NaN
+"true"=>1
+null=0,undefined->NaN
+1=>true
+"akash"=>true
+""=>false
+
+*/
+
+// let isLoggedIn=1
+// let isLoggedIn="akash"
+// let boolIsLoggedIn=Boolean(isLoggedIn)
+// console.log(boolIsLoggedIn);
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
