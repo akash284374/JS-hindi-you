975f78fd94f2b57e77554e86a5d44c18b1b3c12f
akash284374 <121781009+akash284374@users.noreply.github.com>
2024-08-12 05:15:24 +0000

    obj3

---
+++ 02_Basics/object3.js
@@ -0,0 +1,17 @@
+const course={
+    courseName:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+// const {courseInstructor}=course
+const {courseInstructor:instructor}=course
+
+console.log(instructor);
+
+////jason
+// {
+//     "name";"akash",
+//     "courseName";"js",
+//     "price";"free"
+// }
