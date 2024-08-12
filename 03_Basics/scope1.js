4cfd4b4570b71d7800297849f9991b1d9dbc5ba1
akash284374 <121781009+akash284374@users.noreply.github.com>
2024-08-12 11:21:07 +0000

    scope1

---
+++ 03_Basics/scope1.js
@@ -0,0 +1,37 @@
+// let a=10
+// const b=20
+// var c=30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// if(true){
+// // let a=10
+// // const b=20
+// var c=30
+// d=30
+
+// }
+
+// // console.log(a);
+// // console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+
+
+// var c=300
+let a=300
+
+if(true){
+    let a=10
+    const b=20
+
+    console.log("Inner: ",a);
+    }
+    
+    console.log(a);
+    // console.log(b);
+    
