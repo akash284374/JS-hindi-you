f777e5d9509fc88600958a912b3130be1b57de51
akash284374 <121781009+akash284374@users.noreply.github.com>
2024-08-12 10:34:39 +0000

    function2

---
+++ 03_Basics/function2.js
@@ -0,0 +1,34 @@
+// // function calculateCartprice(...num1){
+// function calculateCartprice(val1,val2,...num1){
+//     return num1;
+// }
+// console.log(calculateCartprice(200,400,500,2000));
+
+
+
+
+// const user={
+//     username:"hitesh",
+//     price:199
+
+// }
+// function handleObject(anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+// }
+// // handleObject(user)
+// handleObject({
+//     username:"sam",
+//     price:399
+// })
+
+
+
+
+const newArray=[200,400,100,600]
+function returnSecondvalue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondvalue(newArray));
+console.log(returnSecondvalue([200,400,500,1000]));
+
